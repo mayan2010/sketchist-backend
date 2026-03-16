e6334eed1055530e3b8d5cc8cf94a751f0db0c3d
Mayan <mayan@GolDRoger.local>
2026-03-16 14:42:03 +0530

    Intitial Commit

---
+++ src/test/java/com/example/sketchy/SketchyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sketchy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SketchyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
